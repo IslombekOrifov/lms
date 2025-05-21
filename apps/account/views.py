cc81f6b30806e8c36e8976422e6ea9d3c3bd6392
Islombek Orifov <islombekorifov0199@gmail.com>
2025-05-21 23:22:19 +0500

    login added

--- apps/account/views.py
+++ apps/account/views.py
@@ -6,7 +6,7 @@ from .forms import CustomLoginForm
 
 class CustomLoginView(LoginView):
     form_class = CustomLoginForm
-    template_name = 'accounts/login.html'
+    template_name = 'account/login.html'
     
     def get_success_url(self):
-        return self.request.GET.get('next', '/dashboard/')
+        return self.request.GET.get('next', '/main/')
